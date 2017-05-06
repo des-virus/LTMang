8b6e2a2ea7164b13562a079e69014d6727f62f1c
minhphong306 <dominhphong306@gmail.com>
2017-05-06 12:57:59 +0700

    First commit project

---
+++ LTMang/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTMang {
+    class Program {
+        static void Main(string[] args) {
+            // B1: Khai bao socket server, IPEndPoint, dulieunhan
+            Socket server, client;
+            IPEndPoint IPE_Server, IPE_Client;
+            string receiveString, sendString;
+            int receiveNumber;
+            byte[] receiveBytes, sendBytes;
+
+            // B2: Khoi tao socket server, IPEndPoint
+            IPE_Server = new IPEndPoint(IPAddress.Any, 2017);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // B3: bind IPE cho socket server, so luong listen cung luc
+            server.Bind(IPE_Server);
+            server.Listen(10);
+            Console.WriteLine("Bat dau lang nghe");
+
+            // B4: Chap nhan ket noi tu client
+            client = server.Accept();
+            IPE_Client = (IPEndPoint) client.RemoteEndPoint;
+            Console.WriteLine("Da nhan ket noi tu client: {0}:{1}", IPE_Client.Address, IPE_Client.Port);
+
+            // B5: Nhan du lieu tu client
+            while (true)
+            {
+                receiveBytes = new byte[1024 * 5];
+                sendBytes = new byte[1024 * 5];
+                receiveNumber = client.Receive(receiveBytes);
+                receiveString = Encoding.ASCII.GetString(receiveBytes, 0, receiveNumber);
+                Console.WriteLine($"Nhan duoc chuoi: {receiveString}");
+                sendString = Console.ReadLine();
+                sendBytes = Encoding.ASCII.GetBytes(sendString);
+                client.Send(sendBytes);
+            }
+
+            // B6: Dong ket noi
+            server.Close();
+            client.Close();
+        }
+    }
+}
---
+++ TCPThuong_Client/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPThuong_Client {
+    class Program {
+        static void Main(string[] args) {
+            // B1: Khai bao socket client, IPEndPoint server
+            Socket clientSocket;
+            IPEndPoint IPE_Server;
+            string sendString, receiveString;
+            byte[] receiveBytes, sendBytes;
+            int receiveNumber;
+
+            // B2: Khoi tao socket client, IPEndPoint server
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPE_Server = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2017);
+
+            // B3: Client connect den IPEndPoint cua server
+            clientSocket.Connect(IPE_Server);
+            if (clientSocket.Connected)
+            {
+                Console.WriteLine("Da ket noi thanh cong");
+            }
+            else
+            {
+                Console.WriteLine("Ket noi loi");
+                Console.Read();
+                return;
+            }
+
+            // B4: Doi nguoi dung nhap, gui sang server
+            //      Lang nghe tu server va in chuoi ra
+            while (true) {
+                sendBytes = new byte[1024 * 5];
+                receiveBytes = new byte[1024 * 5];
+                sendString = Console.ReadLine();
+                sendBytes = Encoding.ASCII.GetBytes(sendString);
+                clientSocket.Send(sendBytes);
+                receiveNumber = clientSocket.Receive(receiveBytes);
+                receiveString = Encoding.ASCII.GetString(receiveBytes, 0, receiveNumber);
+                Console.WriteLine($"Da nhan chuoi: {receiveString}");
+            }
+
+            // Dong ket noi
+            clientSocket.Close();
+        }
+    }
+}
